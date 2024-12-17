7dde4b63e9592647dbb5775709b15553892ab538
MaratNazimov <marat.abdurahimov.88@yandex.ru>
2024-12-17 13:38:44 +0700

    Update urls.py

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import class_temp, func_temp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('class/', class_temp),
+    path('fanc/', func_temp),
 ]
