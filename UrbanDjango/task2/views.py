4ef8b64bd2c5de427b046b9fcbf8467f2bdb036f
MaratNazimov <marat.abdurahimov.88@yandex.ru>
2024-12-18 16:25:10 +0700

    Update views.py

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import TemplateView
-# Create your views here.
 
-def class_temp(request):
-    return render(request, 'second_task/class_temp.html')
 
 def func_temp(request):
     return render(request, 'second_task/func_temp.html')
 
+class class_temp(TemplateView):
+    template_name = 'second_task/class_temp.html'
